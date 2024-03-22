5f51819bb84a3a8ba5e52175f9e06504781705af
Jayath Kasiwaththa <it21204234@my.sliit.lk>
2024-03-22 12:57:57 +0530

    Updated

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -31,6 +31,6 @@ export default function OAuth() {
         }
     }
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
+    <button onClick={handleGoogleClick} type='button' className='bg-red-500 text-white p-3 rounded-lg  hover:opacity-95 shadow-lg'>Continue with google</button>
   )
 }
