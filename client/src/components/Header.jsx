aa6eb90d36c02bd7a90f20df75bebb8a4f431c34
KethminiKW <kethu.kavindya@gmail.com>
2024-03-23 02:53:24 +0530

    Create get users API route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import logo from '../images/logo.png'
-
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className='bg-blue-800 shadow-md'>
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <img src={logo} alt="Logo" className="h-24 flex flex-wrap" />
-
+        <Link to='/'>
+          <img src={logo} alt="Logo" className="h-24 flex flex-wrap" />
+        </Link>
+        
         <ul className='flex gap-5 items-center'>
           <li className='hidden sm:inline text-white hover:underline'>Home</li>
           <li className='hidden sm:inline text-white hover:underline'>About</li>
@@ -15,8 +17,13 @@ export default function Header() {
         </ul>
 
         <div className='space-x-4'>
-          <button className='bg-white text-black rounded-lg uppercase hover:opacity-95 p-3 shadow-md'>Register</button>
-          <button className='bg-white text-black rounded-lg uppercase hover:opacity-95 p-3 shadow-md'>Sign in</button>
+          <Link to='/register'>
+            <button className='bg-white text-black rounded-lg uppercase hover:opacity-95 p-3 shadow-md'>Register</button>
+          </Link>
+          <Link to='/signin'>
+            <button className='bg-white text-black rounded-lg uppercase hover:opacity-95 p-3 shadow-md'>Sign in</button>
+          </Link>
+          
         </div>
       </div>
       
