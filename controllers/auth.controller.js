bb76271f37a1ffcaaa2d2b28612fb57f0d8a7844
Jayath Kasiwaththa <it21204234@my.sliit.lk>
2024-03-22 22:33:48 +0530

    Add admin functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,7 +51,10 @@ export const signin = async (req, res, next) => {
       if (!validPassword) {
         return next(errorHandler(400, 'Invalid password'));
       }
-      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: validUser._id, isAdmin: validUser.isAdmin },
+        process.env.JWT_SECRET
+      );
   
       const { password: pass, ...rest } = validUser._doc;
   
@@ -71,7 +74,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email })
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password: pass, ...rest } = user._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
@@ -83,7 +89,10 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4) , email: req.body.email, password: hashedPassword, avatar: req.body.photo });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password: pass, ...rest } = newUser._doc;
       res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
 
